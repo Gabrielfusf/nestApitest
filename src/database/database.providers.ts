2a2a3a110afcd5859185f2c2107e3741b3e0f1e0
Joao gabriel Faria <joaogabrielfaria@leiaassistent.local>
2023-05-27 20:04:34 -0300

    fix: update url bd

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,11 +6,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        host: 'localhost',
         port: 3306,
-        username: 'x5a6tkag2qag1bod',
-        password: 'x6ys966ell7zgq58',
-        database: 'miu4d918o6012lan',
+        username: 'xx',
+        password: 'xx',
+        database: 'xx',
         entities: [__dirname + '/../**/*.entity{.ts,.js}',],
         synchronize: true,
       });
